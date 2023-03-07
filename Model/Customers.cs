09dd0feddb7b950500a476653908c68d00e630f8
Lebohang Sopazi <lebohang.sopazi@firsttech.digital>
2023-03-07 10:29:01 +0200

    Allow users to authenticate

---
+++ Model/Customers.cs
@@ -0,0 +1,8 @@
+﻿namespace Practising_Authentication.Model
+{
+    public class Customers
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
